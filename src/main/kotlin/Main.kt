54f46d2fb6655d632580c705cc1c9568a19ff676
Husoid <husoid@mail.ru>
2023-06-12 21:38:29 +0500

    work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println(myPay("MasterCard", amountTransfer = 7000))
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun myPay(typeCard: String = "VK pay", amountTransfersInMonth: Int = 0, amountTransfer: Int): String {
+    var commission: Int
+
+    when (typeCard) {
+        "MasterCard", "Maestro" ->
+            if (amountTransfersInMonth + amountTransfer > 75000) {
+                commission = ((75000 - amountTransfersInMonth + amountTransfer) * 60 / 100 + 20)
+            } else {
+                commission = (amountTransfer * 60 / 100 + 20)
+            }
+        "VK pay" -> commission = 0
+        else -> commission = 0
+    }
+    return "Ваш перевод на карту $typeCard в сумме $amountTransfer рублей будет с коммиссией $commission рублей"
 }
